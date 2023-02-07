49800f4cefe34238d670e2d592735969f0241c1c
Prashant Sharma <prashanttonusharma@gmail.com>
2023-02-07 23:54:24 +0530

    07-2-23

---
+++ js practice/index.js
@@ -0,0 +1,23 @@
+console.log('pk'); 
+
+var b = 1;
+let a = 1001;
+var b = 2;
+// const num = 12;
+// const num = 222;
+
+
+// we can only change the value of var not let and const keyword
+console.log(b);
+console.log(a);
+
+let person = {
+    firstName : 'pk',
+    age : 20
+};
+
+// array 
+let names = [ 'prashant' , 'keshav'];
+
+console.log(names[1]);
+
