80a223560abd5c7772c925e6ea73246a5b370357
Prashant Sharma <prashanttonusharma@gmail.com>
2023-02-10 13:09:12 +0530

    10-02-23

--- js practice/index.js
+++ js practice/index.js
@@ -1,23 +1,37 @@
 console.log('pk'); 
 
-var b = 1;
-let a = 1001;
-var b = 2;
-// const num = 12;
-// const num = 222;
+// var b = 1;
+// let a = 1001;
+// var b = 2;
+// // const num = 12;
+// // const num = 222;
 
 
-// we can only change the value of var not let and const keyword
-console.log(b);
-console.log(a);
+// // we can only change the value of var not let and const keyword
+// console.log(b);
+// console.log(a);
  
-let person = {
-    firstName : 'pk',
-    age :  20
-};
+// let person = {
+//     firstName : 'pk',
+//     age :  20
+// };
 
-// array 
-let names = [ 'prashant' , 'keshav'];
+// // array 
+// let names = [ 'prashant' , 'keshav'];
 
-console.log(names[1]);
+// console.log(names[1]);
+
+function rectangle1(length , bredth){
+    let rectangle = {
+        len:length,
+        bre:bredth,
+    
+        draw(){
+            console.log("Keshav");
+        }
+    };
+    return rectangle;
+}
+
+let r = rectangle1(2,4);
 
